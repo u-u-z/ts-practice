695be283850daddadc1cf4dc7a96a9484c5d89aa
RemiIO <i@linux.dog>
2019-06-20 18:43:41 +0800

    update for symbol

--- src/index.ts
+++ src/index.ts
@@ -67,7 +67,7 @@ const getLength = (target: string | number) => {
  * Symbol
  */
 
- //獨一無二的值
+//獨一無二的值
 
 const s1 = Symbol('lison')
 const s2 = Symbol(123)
@@ -77,17 +77,17 @@ console.log(Boolean(s2)) // true
 console.log(!s2)//false
 
 const s5 = Symbol('pop')
-let sttr= "test"
+let sttr = "test"
 const a1InfoObj = {
     name: 'lison',
-    [`my${sttr}`]:"",
-    [s5]:'test', //獨一無二不可修改只能如下修改
+    [`my${sttr}`]: "",
+    [s5]: 'test', //獨一無二不可修改只能如下修改
 }
-a1InfoObj[s5]='haha'
+a1InfoObj[s5] = 'haha'
 //for in 的時候遍歷不出來
 //Object.keys() 也獲取不到Symbol
 //Object.getOwnPropertyNames() 獲取不到Symbol
-//JSON.stringify(a1InfoObj) 獲取不到Symbol
+//JSON.stringify(a1InfoObj) //對象轉字符串獲取不到Symbol
 //只有
 console.log(Object.getOwnPropertySymbols(a1InfoObj))
 console.log(Reflect.ownKeys(a1InfoObj)) //可以獲取所有包括symbol
@@ -103,6 +103,7 @@ const s7 = Symbol('zhizhang')
 const s8 = Symbol.for('zhizhang')
 //在全局範圍搜索是否有 創建 zhizhang 字符串的 Symbol
 const s9 = Symbol.for('zhizhang')
+//全局包括頁面、iframe、serviceWork
 
 //s8 === s9 true
 
@@ -118,9 +119,113 @@ Symbol.keyFor(s8) // 傳入 Symbol.for 在全局註冊的標識,如果是 Symbol
 //當你給一個對象 設置 Symbol.hasInstance 屬性名的時候
 //
 const objSy1 = {
-    [Symbol.hasInstance] (otherObj:any) {
+    [Symbol.hasInstance](otherObj: any) {
         console.log(otherObj)
     }
 }
 
+console.log({ a: 'a' } instanceof <any>objSy1)
+//{a:'a'}不是 objSy1 創建的實例
 
+
+//isConcatSpreadable 設置為 true
+// [1,2,3,4] 若不是 [Array(2),3,4]
+//  扁平化操作
+//let arr4 = [1, 2]
+//console.log([].concat(arr4, [3, 4]))
+//arr4[Symbol.isConcatSpreadable] = false //可讀寫的布爾值
+//console.log([].concat(arr4, [3, 4]))
+
+
+
+class C extends Array {
+    constructor(...args: any) {
+        super(...args)
+    }
+    static get [Symbol.species]() {
+        return Array
+    }
+    getName() {
+        return 'jack'
+    }
+}
+
+const c = new Array(1, 2, 3)
+const d = new C(1, 2, 3)
+
+const a = c.map(item => {
+    return item + 1
+})
+const b = d.map((item: number) => {
+    return item + 1
+})
+// a [2,3,4] a是c的延伸數據
+// c instanceof Array //true
+
+// a instanceof C //JS/TS false 去掉Symbol.species]true
+// a instanceof Array //true
+/*
+    用 Symbol.species 來指定創建衍生對象的對象函數
+*/
+
+let obj3 = {
+    [Symbol.match](string: string) {
+        console.log(string.length)
+    },
+    [Symbol.replace](string: string) {
+        console.log(string.length)
+    }
+}
+'abcde'.match(<RegExp><unknown>obj3)
+//5
+
+//Symbol.replace 
+//Symbol.search
+//Symbol.split
+
+
+//////////////
+
+// Symbol.iterator 返回 數組的遍歷器
+const arr5: number[] = [1, 2, 3, 4]
+const iterator = arr5[Symbol.iterator]()
+iterator.next() //{value:1,done:false}
+iterator.next() //{value:2,done:false}
+iterator.next() //{value:3,done:false}
+iterator.next() //{value:4,done:false}
+iterator.next() //{value:undefined,done:true}
+
+let obj4: unknown = {
+    [Symbol.toPrimitive](type: any) {
+        console.log(type)
+    }
+}
+
+// const res1 = (obj4 as number)++
+const res2 = `abc${obj4}`  
+
+
+
+//===================
+//Symbol.toStringTag
+let obj5 = {
+    [Symbol.toStringTag]:'remi'
+}
+let obj51 = {
+    get [Symbol.toStringTag](){
+        return 'remi'
+    }
+}
+
+console.log(obj5.toString()) //[object remi]
+console.log(obj51.toString()) //[object remi]
+
+const objExp1 =  {
+    a:'test1',
+    b:'test2'
+}
+
+console.log(Array.prototype[Symbol.unscopables])
+// with(<any>objExp1){
+//     console.log()
+// }
